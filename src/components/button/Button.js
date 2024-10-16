b6688349e4b02f5f44d53d3222669b4b9a0f6af0
beka lekishvili <58632301+Tilted-One@users.noreply.github.com>
2024-10-16 22:42:16 +0400

    fix

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 export default function Button(props) {
+    const attribute ={
+        'all': 'Universal',
+        'str': 'Strength',
+        'agi': 'Agility',
+        'int': 'Intelligence'
+    }
     return (
-        <button value={props.value} className={`${props.butt} ${(props.state == props.value ? props.buttActive : '')}`} title={props.value} onClick={() => props.setState(props.state == props.value ? null : props.value)} >
+        <button value={props.value} className={`${props.butt} ${(props.state == props.value ? props.buttActive : '')}`} title={attribute[props.value]} onClick={() => props.setState(props.state == props.value ? null : props.value)} >
             <img src={props.icon} alt={props.value} ></img>
         </button>
     )
